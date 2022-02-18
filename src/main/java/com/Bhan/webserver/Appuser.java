026fcf291184631df5dcde3e868b9ab546c7eff1
Bhan <tanwar.c@northeastern.edu>
2022-02-18 04:47:06 -0500

    First commit for aasignment 2

---
+++ src/main/java/com/Bhan/webserver/Appuser.java
@@ -0,0 +1,82 @@
+package com.Bhan.webserver;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity(name="users")
+@Table(name="users")
+public class Appuser {
+    @Id
+    private String id;
+    private String first_name;
+    private String last_name;
+    private String username;
+    private LocalDateTime account_created;
+    private LocalDateTime account_updated;
+    @JsonIgnore
+    private String password;
+
+    public Appuser(String first_name, String last_name, String username, String password) {
+        this.id = UUID.randomUUID().toString();
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.username = username;
+        this.account_created = LocalDateTime.now();
+        this.account_updated = LocalDateTime.now();
+        this.password = password;
+    }
+
+    public Appuser(Createuser newuser) {
+
+        this.id = UUID.randomUUID().toString();
+        this.first_name = newuser.getFirst_name();
+        this.last_name = newuser.getLast_name();
+        this.username = newuser.getUsername();
+        this.account_created = LocalDateTime.now();
+        this.account_updated = LocalDateTime.now();
+        this.password = newuser.getPassword();
+    }
+
+    public Appuser() {
+    }
+
+    public String getId() {return id;}
+    public void setId(String id) {this.id = id;}
+
+    public String getFirst_name() {return first_name;}
+    public void setFirst_name(String first_name) {this.first_name = first_name;}
+
+    public String getLast_name() {return last_name;}
+    public void setLast_name(String last_name) {this.last_name = last_name;}
+
+    public String getUsername() {return username;}
+    public void setUsername(String username) {this.username = username;}
+
+    public LocalDateTime getAccount_created() {return account_created;}
+    public void setAccount_created(LocalDateTime account_created) {this.account_created = account_created;}
+
+    public LocalDateTime getAccount_updated() {return account_updated;}
+    public void setAccount_updated(LocalDateTime account_updated) {this.account_updated = account_updated;}
+
+    public String getPassword() {return password;}
+    public void setPassword(String password) {this.password = password;}
+
+    @Override
+    public String toString() {
+        return "Appuser{" +
+                "id='" + id + '\'' +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", username='" + username + '\'' +
+                ", account_created=" + account_created +
+                ", account_updated=" + account_updated +
+                '}';
+    }
+
+    public void accountupdate(){this.account_updated = LocalDateTime.now();}
+}
--- src/main/java/com/Bhan/webserver/Maincontroller.java
+++ src/main/java/com/Bhan/webserver/Maincontroller.java
@@ -1,14 +1,18 @@
 package com.Bhan.webserver;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Base64;
 
 @RestController
 public class Maincontroller {
+    @Autowired
+    private Userrepository userrepository;
 
     @GetMapping(path = "/healthz", produces = MediaType.APPLICATION_JSON_VALUE)
     //@ResponseStatus(HttpStatus.OK)
@@ -16,6 +20,71 @@ public class Maincontroller {
         Healthzresponse response = new Healthzresponse("Success");
         return new ResponseEntity<Healthzresponse>(response, HttpStatus.OK);
 
+    }
+
+    @PostMapping(path = "/v1/user", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Appuser> createuser(@Valid @RequestBody Createuser newuser){
+        Appuser user = new Appuser(newuser);
+        userrepository.save(user);
+        return new ResponseEntity<Appuser>(user, HttpStatus.CREATED);
+    }
+
+    @PutMapping(path = "/v1/user/self", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> updateuser(@RequestBody Updateuser updateuser, @RequestHeader("Authorization") String authheader){
+        String[] authtokens = authheader.split(" ");
+        if (authtokens[0].equals("Basic")){
+            byte[] decodedBytes = Base64.getDecoder().decode(authtokens[1]);
+            String authvalues = new String(decodedBytes);
+            String[] authcreds = authvalues.split(":");
+
+            Appuser user = userrepository.finduserbyusername(authcreds[0]);
+            boolean fields = false;
+            if (user.getPassword().equals(authcreds[1])){
+                if (updateuser.getUsername() != null){
+                    user.setUsername(updateuser.getUsername());
+                    fields = true;
+                }
+                if (updateuser.getFirst_name() != null){
+                    user.setFirst_name(updateuser.getFirst_name());
+                    fields = true;
+
+                }
+                if (updateuser.getLast_name() != null){
+                    user.setLast_name(updateuser.getLast_name());
+                    fields = true;
+
+                }
+                if (updateuser.getPassword() != null){
+                    user.setPassword(updateuser.getPassword());
+                    fields = true;
+                }
+                if (!fields){
+                    return new ResponseEntity(HttpStatus.BAD_REQUEST);
+                }
+                user.accountupdate();
+                userrepository.save(user);
+                return new ResponseEntity<String>("Updated", HttpStatus.NO_CONTENT);
+            }
+        }
+        return new ResponseEntity<String>("Unauthorized", HttpStatus.UNAUTHORIZED);
+    }
+
+
+    @GetMapping(path = "/v1/user/self", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> showuser(@RequestHeader("Authorization") String authheader){
+        String[] authtokens = authheader.split(" ");
+        if (authtokens[0].equals("Basic")){
+            byte[] decodedBytes = Base64.getDecoder().decode(authtokens[1]);
+            String authvalues = new String(decodedBytes);
+            String[] authcreds = authvalues.split(":");
+
+            Appuser user = userrepository.finduserbyusername(authcreds[0]);
+
+            if (user.getPassword().equals(authcreds[1])){
+                return new ResponseEntity<Appuser>(user, HttpStatus.OK);
+            }
+        }
+        return new ResponseEntity<String>("Unauthorized", HttpStatus.UNAUTHORIZED);
     }
 
 }
