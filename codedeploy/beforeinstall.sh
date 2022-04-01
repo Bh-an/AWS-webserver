e59c499c27c0f717f8e3a1769befce934e4b192f
Bhan <tanwar.c@northeastern.edu>
2022-04-01 17:01:59 -0400

    Updated codedeploy scripts

--- codedeploy/beforeinstall.sh
+++ codedeploy/beforeinstall.sh
@@ -4,3 +4,4 @@ sudo systemctl stop csye6225app.service
 
 [ ! -f /home/ec2-user/javaapp/webserver-0.0.1-SNAPSHOT.jar ] || sudo mv /home/ec2-user/javaapp/webserver-0.0.1-SNAPSHOT.jar /home/ec2-user/backups/$(date +"%Y_%m_%d_%I_%M_%p").jar
 
+sudo rm -rf /home/ec2-user/javaapp/webserver-0.0.1-SNAPSHOT.jar
