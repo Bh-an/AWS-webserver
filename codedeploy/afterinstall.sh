f9a47c9216d6674d183763f4a76b0b450750140b
Bhan <tanwar.c@northeastern.edu>
2022-04-01 14:09:19 -0400

    Updated codedeploy scripts

--- codedeploy/afterinstall.sh
+++ codedeploy/afterinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo rm -rf ~/javaapp/webserver-0.0.1-SNAPSHOT.jar
+sudo rm -rf /home/ec2-user/javaapp/webserver-0.0.1-SNAPSHOT.jar
 
-sudo chown ec2-user ~/webserver-0.0.1-SNAPSHOT.jar
+sudo chown ec2-user /home/ec2-user/webserver-0.0.1-SNAPSHOT.jar
 
-sudo mv ~/webserver-0.0.1-SNAPSHOT.jar ~/javaapp/webserver-0.0.1-SNAPSHOT.jar
+sudo mv /home/ec2-user/webserver-0.0.1-SNAPSHOT.jar /home/ec2-user/javaapp/webserver-0.0.1-SNAPSHOT.jar
--- codedeploy/beforeinstall.sh
+++ codedeploy/beforeinstall.sh
@@ -3,5 +3,5 @@
 sudo systemctl stop csye6225app.service
 
 backup=${PWD##*/}
-sudo mv ~/javaapp/webserver-0.0.1-SNAPSHOT.jar ~/backups/$backup.jar
+sudo mv /home/ec2-user/javaapp/webserver-0.0.1-SNAPSHOT.jar /home/ec2-user/backups/$backup.jar
 
