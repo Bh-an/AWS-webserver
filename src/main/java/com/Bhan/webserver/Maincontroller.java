55382cd50b72383dc6339b6a6482d37c9313f78c
Bhan <tanwar.c@northeastern.edu>
2022-04-07 15:36:27 -0400

    Added metric dimensions

--- src/main/java/com/Bhan/webserver/Maincontroller.java
+++ src/main/java/com/Bhan/webserver/Maincontroller.java
@@ -37,7 +37,7 @@ public class Maincontroller {
 
     private static final Logger logger = LoggerFactory.getLogger(Maincontroller.class);
 
-    @GetMapping(path = "/healthz", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(path = "/health", produces = MediaType.APPLICATION_JSON_VALUE)
     //@ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Healthzresponse> test() {
         statsd.incrementCounter("server.get.healthz");
