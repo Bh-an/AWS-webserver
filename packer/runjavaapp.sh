2c0dea9ddbc9183dbc567640315c93cc305bbafe
Bhan <tanwar.c@northeastern.edu>
2022-04-22 20:23:23 -0400

    Building demo packer image

--- packer/runjavaapp.sh
+++ packer/runjavaapp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -Djavax.net.ssl.trustStore=/home/ec2-user/javaapp/mytruststore.jks -Djavax.net.ssl.trustStorePassword=hesoyam -Djavax.net.ssl.keyStore=/home/ec2-user/javaapp/mytruststore.jks -Djavax.net.ssl.keyStorePassword=hesoyam -jar webserver-0.0.1-SNAPSHOT.jar
+java -jar webserver-0.0.1-SNAPSHOT.jar
